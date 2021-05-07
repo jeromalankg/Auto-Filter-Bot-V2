a87cb49e48e6fa8605b55d743b87916255b30a56
jeromalankg <73247428+jeromalankg@users.noreply.github.com>
2021-05-07 11:21:46 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -41,7 +41,7 @@ async def filter(client: Bot, message: Message):
         if filenames and links:
             for filename, link in zip(filenames, links):
                 btn.append(
-                    [InlineKeyboardButton(f"📺"+text=f"{filename}",url=f"{link}")]
+                    [InlineKeyboardButton(text=f"📺"+{filename}",url=f"{link}")]
                 )
         else:
             return
